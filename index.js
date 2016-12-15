7d32170e7e68279b7df78bb807387e68ef26c27c
Data Entry User <Data Entry User>
2016-12-15 11:58:41 -0500

    file download successful

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-var request = require('curlrequest');
-var urlencode = require('urlencode');
-
+var request = require('request');
+var fs = require('fs');
 var token = require('./token.js');
-var url_encoded_token = urlencode(token.Token);
+
 
 
 var options = {
   url: 'https://bulksell.ebay.com/ws/eBayISAPI.dll?FileExchangeProgrammaticDownload',
   headers: {
     'User-Agent': 'request',
-    'Connection': 'Keep-Alive',
-    'RefId': process.argv[2],
-    'token': url_encoded_token
-    
+    'Connection': 'Keep-Alive',  
   }
 };
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage. 
-  }
+var idform = {
+    token:token.Token,
+    refId:process.argv[2]
+};
+
+request.post({url: options.url, form: idform}, function (error, response, body) {
+  if (error) throw error;
+    fs.writeFile('node_sold_ebay.csv', body, (err) => {
+      if (err) throw err; 
+      console.log("file saved....");
+    });
 });
