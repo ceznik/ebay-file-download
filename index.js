3f709d3c680ec646d53b913cb3c812b7facfae3f
Data Entry User <Data Entry User>
2017-01-12 09:58:25 -0500

    2016 backup

--- index.js
+++ index.js
@@ -8,18 +8,31 @@ var options = {
   url: 'https://bulksell.ebay.com/ws/eBayISAPI.dll?FileExchangeProgrammaticDownload',
   headers: {
     'User-Agent': 'request',
-    'Connection': 'Keep-Alive',  
+    'Connection': 'Keep-Alive',
   }
 };
 var idform = {
     token:token.Token,
-    refId:process.argv[2]
+    refId:process.argv[3]
 };
 
 request.post({url: options.url, form: idform}, function (error, response, body) {
   if (error) throw error;
-    fs.writeFile('node_sold_ebay.csv', body, (err) => {
-      if (err) throw err; 
-      console.log("file saved....");
-    });
+  switch(process.argv[2]){
+    case "sold":
+      fs.writeFile('node_sold_ebay.csv', body, (err) => {
+        if (err) throw err; 
+        console.log("sold listings file saved....");      
+      });
+      break;
+    case "unsold":
+      fs.writeFile('node_unsold_ebay.csv', body, (err) => {
+        if (err) throw err;
+        console.log("unsold listings file saved....");
+      });
+      break;
+    default:
+      console.log("arguments not recognized");
+      break;
+  }
 });
